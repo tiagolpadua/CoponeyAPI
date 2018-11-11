046d01267b08e693ebffa92a1f7d8b22a8f10a09
tiago <tiagolpadua@gmail.com>
2018-11-11 15:30:28 -0200

    exclusão lógica

--- app/api/index.js
+++ app/api/index.js
@@ -1,51 +1,66 @@
-var db = require('../../config/database');
+var db = require("../../config/database");
 
-var api = {}
+var api = {};
 
-api.adiciona = function (req, res) {
-    var poney = req.body;
-    console.log("Adicionando: ");
-    console.log(req.body);
-    delete poney._id;
-    db.insert(poney, function (err, newDoc) {
-        if (err) return console.log(err);
-        console.log('Adicionado com sucesso: ' + newDoc._id);
-        res.json(newDoc._id);
+api.adiciona = function(req, res) {
+  var poney = req.body;
+  console.log("Adicionando: ");
+  console.log(req.body);
+  delete poney._id;
+  db.insert(poney, function(err, newDoc) {
+    if (err) return console.log(err);
+    console.log("Adicionado com sucesso: " + newDoc._id);
+    res.json(newDoc._id);
+  });
+};
+
+api.busca = function(req, res) {
+  db.findOne({ _id: req.params.poneyId }, function(err, doc) {
+    if (err) return console.log(err);
+    res.json(doc);
+  });
+};
+
+api.atualiza = function(req, res) {
+  console.log("Parâmetro recebido:" + req.params.poneyId);
+  db.update({ _id: req.params.poneyId }, req.body, function(err, numReplaced) {
+    if (err) return console.log(err);
+    if (numReplaced) res.status(200).end();
+    res.status(500).end();
+    console.log("Atualizado com sucesso: " + req.body._id);
+    res.status(200).end();
+  });
+};
+
+api.lista = function(req, res) {
+  db.find({})
+    .sort({ nome: 1 })
+    .exec(function(err, doc) {
+      if (err) return console.log(err);
+      res.json(doc);
     });
 };
 
-api.busca = function (req, res) {
-    db.findOne({ _id: req.params.poneyId }, function (err, doc) {
-        if (err) return console.log(err);
-        res.json(doc);
-    });
+api.remove = function(req, res) {
+  db.remove({ _id: req.params.poneyId }, {}, function(err, numRemoved) {
+    if (err) return console.log(err);
+    console.log("removido com sucesso");
+    if (numRemoved) res.status(200).end();
+    res.status(500).end();
+  });
 };
 
-api.atualiza = function (req, res) {
-    console.log('Parâmetro recebido:' + req.params.poneyId);
-    db.update({ _id: req.params.poneyId }, req.body, function (err, numReplaced) {
-        if (err) return console.log(err);
-        if (numReplaced) res.status(200).end();
-        res.status(500).end();
-        console.log('Atualizado com sucesso: ' + req.body._id);
-        res.status(200).end();
-    });
-};
-
-api.lista = function (req, res) {
-    db.find({}).sort({ nome: 1 }).exec(function (err, doc) {
-        if (err) return console.log(err);
-        res.json(doc);
-    });
-};
-
-api.remove = function (req, res) {
-    db.remove({ _id: req.params.poneyId }, {}, function (err, numRemoved) {
-        if (err) return console.log(err);
-        console.log('removido com sucesso');
-        if (numRemoved) res.status(200).end();
-        res.status(500).end();
-    });
+api.removeLogicamente = function(req, res) {
+  db.update({ _id: req.params.poneyId }, { $set: { excluido: true } }, function(
+    err,
+    numReplaced
+  ) {
+    if (err) return console.log(err);
+    if (numReplaced) res.status(200).end();
+    res.status(500).end();
+    console.log("Excluído logicamente com sucesso: " + req.params.poneyId);
+    res.status(200).end();
+  });
 };
 
 module.exports = api;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,18 +1,21 @@
-var api = require('../api'),
-    path = require('path');
+var api = require("../api"),
+  path = require("path");
 
-module.exports  = function(app) {
-    
-    app.route('/v1/poneys')
-        .post(api.adiciona)
-        .get(api.lista);
+module.exports = function(app) {
+  app
+    .route("/v1/poneys")
+    .post(api.adiciona)
+    .get(api.lista);
 
-    app.route('/v1/poneys/:poneyId')
-        .delete(api.remove)
-        .get(api.busca)
-        .put(api.atualiza);
+  app
+    .route("/v1/poneys/:poneyId")
+    .delete(api.remove)
+    .get(api.busca)
+    .put(api.atualiza);
 
-    app.all('/*', function(req, res) {
-        res.sendFile(path.join(app.get('publicPath'), 'index.html'));
-    });
+  app.route("/v1/poneys/exclusaologica/:poneyId").delete(api.removeLogicamente);
+
+  app.all("/*", function(req, res) {
+    res.sendFile(path.join(app.get("publicPath"), "index.html"));
+  });
 };
